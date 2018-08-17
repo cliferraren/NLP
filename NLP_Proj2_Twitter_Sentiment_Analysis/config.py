ed41109adc512db90d596fe4c059dfef39f29361
Clifford <cliferraren@gmail.com>
2018-08-16 22:46:15 -0700

    Twitter credentials needed for authentication

--- NLP_Proj2_Twitter_Sentiment_Analysis/config.py
+++
@@ -1,5 +0,0 @@
-# Twitter API Keys
-consumer_key = '03Yii74shopdyPxXTLKIoV6hG'
-consumer_secret = 'fWqJmJDPEd6dpApu4zHaADtwviv21XcP38Z9qpGI8hzU23Z4kz'
-access_token = '973447287656427520-8FNeGTlkndKBjCUeCMXKAlxstNbL2tM'
-access_token_secret = 'J0nWBz6uaGn41FiTgKaumWLC4RuCTZ7GlUWoFKZkwjmVz'
